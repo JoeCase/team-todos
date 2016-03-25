e45c503ef0c345ec752ab546b41afa1e91aa7f6b
Joe Case <jcase146@gmail.com>
2016-03-24 21:15:48 -0400

    Bootrap. Edit Routes for members and users

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,17 +7,41 @@ class MembersController < ApplicationController
   end
 
   def new
+    @member = Member.new
   end
 
   def create
+    @member = Member.new(member_params.merge(user: current_user))
+    # binding.pry
+    if @member.save
+      flash[:notice] = "It was saved!"
+      redirect_to user_path(current_user)
+    else
+      flash[:notice] = "There was a problem"
+      render new_task_path
+    end
   end
 
   def update
+    @member = Member.find(params[:id])
+    if @member.update_attributes(member_params)
+      redirect_to member_path
+    else
+      flash[:notice] = "There was a problem"
+    end
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def destroy
   end
+
+
+  private
+  def member_params
+    params.require(:member).permit(:first_name, :last_name, :email, :phone )
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit!)
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(current_user)
@@ -25,11 +25,26 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path
+    else
+      flash[:notice] = "There was a problem"
+    end
+
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :description, :password )
+  end
+
+
 end
